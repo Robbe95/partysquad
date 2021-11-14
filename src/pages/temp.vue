680f43bfdfd1fe028789238c99063e089821ed86
Robbe Vaes <robbevaes95@gmail.com>
2021-11-14 19:53:31 +0100

    Loading stuff

--- src/pages/temp.vue
+++ src/pages/temp.vue
@@ -149,92 +149,129 @@ onMounted(() => {
     loading.value = false
 
     setupAnimations()
-  }, 1000)
+  }, 2000)
 
   // setupOrbit()
 })
 </script>
 
 <template>
-  <div v-show="loading">
-    LOADING
-  </div>
-  <div v-show="!loading" ref="container" class="bg-black h-screen min-w-screen relative overflow-hidden flex items-center justify-center">
-    <div class="flex items-center justify-center w-full h-full absolute" target="0">
-      <img id="title" class="z-20" src="@/assets/title.png" />
-    </div>
-
-    <img class="absolute w-full h-full object-cover" src="@/assets/background.png" />
-    <img
-      v-for="i in 20"
-      :id="'cow-' +i"
-      :key="i"
-      class="absolute z-0 cow w-auto -top-2/8 right-0 scale-50"
-      target="0"
-      src="@/assets/cow.png"
-    />
-
-    <!-- <div id="orbit" class="orbit z-0 h-80 w-80 mt-80 ml-80">
-      <img class="transform z-" src="@/assets/maarten.png" />
-    </div> -->
-    <div id="unicorn-wings" class="absolute bottom-0 left-30 z-20">
-      <div class="relative rotate-y-180 transform">
-        <img id="unicorn" class="h-180" src="@/assets/unicorn.png" />
-        <img id="wings" class="absolute -top-20 -left-12 -z-10 h-120" src="@/assets/wings.png" />
-        <img id="glow" class="absolute top-5 left-24 -z-10 h-40 opacity-40 opacity-0" src="@/assets/glow.png" />
-      </div>
-    </div>
-
-    <div class="absolute bottom-0 left-40">
-      <img id="maarten" class="" src="@/assets/maarten.png" />
-      <div class="hover:scale-110 transform transition absolute -top-14 left-10 cursor-pointer" @click="removePoop">
-        <img id="poop" class="h-30" src="@/assets/poop.png" />
-      </div>
-    </div>
-    <img id="robbe" class="absolute bottom-0 left-0" src="@/assets/robbe.png" />
-    <div class="absolute bottom-1/2 left-0 z-10">
-      <div class="transform hover:scale-110 transition" target="1">
-        <img id="joren" src="@/assets/joren.png" class="cursor-pointer" @click="jorenGetsBeer" />
-      </div>
-      <div class="absolute left-10 -bottom-10">
-        <div
-          class="bubble bubble-bottom-left "
-        >
-          Zal ik wa pintjes halen?
+  <div class="relative">
+    <transition name="fade">
+      <div v-if="loading" class="w-screen h-screen absolute top-0 left-0 z-50">
+        <div class="loading w-full h-full flex items-center justify-center">
+          <div class="text-3xl text-white">
+            Loading...
+          </div>
         </div>
       </div>
-    </div>
-    <div class="absolute bottom-1/2 -left-0 h-80 rotate-90 transform">
-      <img id="bierjoren" class="h-full" src="@/assets/bierjoren.png" />
-    </div>
-    <img id="fish" class="absolute bottom-1/8 left-1/2" src="@/assets/fish.png" />
+    </transition>
 
-    <div class="absolute bottom-1/2 left-1/4 hover:scale-110 transition transform cursor-pointer">
-      <img id="ellen" class=" " src="@/assets/ellen.png" @click="moveEllen" />
-    </div>
-    <img id="sm" class="absolute bottom-0 right-25 z-10" src="@/assets/sm.png" />
-    <div class="absolute bottom-40 right-20 z-0">
-      <div class="relative">
-        <img id="leander" class="" src="@/assets/leander.png" />
-        <img id="banana" class="absolute -left-24 top-40 -z-10" src="@/assets/banana.png" />
+    <div ref="container" class="bg-black h-screen min-w-screen relative overflow-hidden flex items-center justify-center">
+      <div class="flex items-center justify-center w-full h-full absolute" target="0">
+        <img id="title" class="z-20" src="@/assets/title.png" />
       </div>
-      <img class="h-8 transform rotate-y-180 absolute top-17 right-28" src="@/assets/tongue.gif" />
-    </div>
-    <div>
-      <img id="star1" class="absolute top-1/6 left-5/12" src="@/assets/star.svg" />
-      <img id="star2" class="absolute top-7/12 left-1/12 transform rotate-54" src="@/assets/star.svg" />
-      <img id="star3" class="absolute top-10/12 left-4/12 transform rotate-34" src="@/assets/star.svg" />
-      <img id="star4" class="absolute top-11/12 left-6/12 transform rotate-170" src="@/assets/star.svg" />
-      <img id="star5" class="absolute top-6/12 left-8/12 transform rotate-120" src="@/assets/star.svg" />
+
+      <img class="absolute w-full h-full object-cover" src="@/assets/background.png" />
+      <img
+        v-for="i in 20"
+        :id="'cow-' +i"
+        :key="i"
+        class="absolute z-0 cow w-auto -top-2/8 right-0 scale-50"
+        target="0"
+        src="@/assets/cow.png"
+      />
+
+      <!-- <div id="orbit" class="orbit z-0 h-80 w-80 mt-80 ml-80">
+      <img class="transform z-" src="@/assets/maarten.png" />
+    </div> -->
+      <div id="unicorn-wings" class="absolute bottom-0 left-30 z-20">
+        <div class="relative rotate-y-180 transform">
+          <img id="unicorn" class="h-180" src="@/assets/unicorn.png" />
+          <img id="wings" class="absolute -top-20 -left-12 -z-10 h-120" src="@/assets/wings.png" />
+          <img id="glow" class="absolute top-5 left-24 -z-10 h-40 opacity-40 opacity-0" src="@/assets/glow.png" />
+        </div>
+      </div>
+
+      <div class="absolute bottom-0 left-40">
+        <img id="maarten" class="" src="@/assets/maarten.png" />
+        <div class="hover:scale-110 transform transition absolute -top-14 left-10 cursor-pointer" @click="removePoop">
+          <img id="poop" class="h-30" src="@/assets/poop.png" />
+        </div>
+      </div>
+      <img id="robbe" class="absolute bottom-0 left-0" src="@/assets/robbe.png" />
+      <div class="absolute bottom-1/2 left-0 z-10">
+        <div class="transform hover:scale-110 transition" target="1">
+          <img id="joren" src="@/assets/joren.png" class="cursor-pointer" @click="jorenGetsBeer" />
+        </div>
+        <div class="absolute left-10 -bottom-10">
+          <div
+            class="bubble bubble-bottom-left "
+          >
+            Zal ik wa pintjes halen?
+          </div>
+        </div>
+      </div>
+      <div class="absolute bottom-1/2 -left-0 h-80 rotate-90 transform">
+        <img id="bierjoren" class="h-full" src="@/assets/bierjoren.png" />
+      </div>
+      <img id="fish" class="absolute bottom-1/8 left-1/2" src="@/assets/fish.png" />
+
+      <div class="absolute bottom-1/2 left-1/4 hover:scale-110 transition transform cursor-pointer">
+        <img id="ellen" class=" " src="@/assets/ellen.png" @click="moveEllen" />
+      </div>
+      <img id="sm" class="absolute bottom-0 right-25 z-10" src="@/assets/sm.png" />
+      <div class="absolute bottom-40 right-20 z-0">
+        <div class="relative">
+          <img id="leander" class="" src="@/assets/leander.png" />
+          <img id="banana" class="absolute -left-24 top-40 -z-10" src="@/assets/banana.png" />
+        </div>
+        <img class="h-8 transform rotate-y-180 absolute top-17 right-28" src="@/assets/tongue.gif" />
+      </div>
+      <div>
+        <img id="star1" class="absolute top-1/6 left-5/12" src="@/assets/star.svg" />
+        <img id="star2" class="absolute top-7/12 left-1/12 transform rotate-54" src="@/assets/star.svg" />
+        <img id="star3" class="absolute top-10/12 left-4/12 transform rotate-34" src="@/assets/star.svg" />
+        <img id="star4" class="absolute top-11/12 left-6/12 transform rotate-170" src="@/assets/star.svg" />
+        <img id="star5" class="absolute top-6/12 left-8/12 transform rotate-120" src="@/assets/star.svg" />
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
+
+.body {
+  background-color: purple;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter-from, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+.loading {
+    background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+    background-size: 400% 400%;
+    animation: gradient 15s ease infinite;
+}
+
+@keyframes gradient {
+    0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
+}
+
 @for $i from 1 through 20 {
   #cow-#{$i} {
-    animation: cow-animation 2s 0s 1 normal forwards;
-    animation-delay: 20ms * $i;
+    animation: cow-animation 2s 1 normal forwards;
+    animation-delay: 20ms * $i + 2s;
     opacity: 0;
   }
 }
